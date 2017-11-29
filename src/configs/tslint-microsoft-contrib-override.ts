cacb787f5177367b20930bc96e0c2abc530e4a15
srg985 <srg985@mail.ee>
2017-11-29 14:41:56 +0300

    feat(tslint-config): TSLint corrections + legacy compatibility rules (#3)
    feat(tslint-config): TSLint corrections + legacy compatibility rules (#3)

--- src/configs/tslint-microsoft-contrib-override.ts
+++ src/configs/tslint-microsoft-contrib-override.ts
@@ -3,10 +3,12 @@ export default {
     rules: {
         // Maintainability
         "export-name": false,
+        "function-name": false,
         "missing-jsdoc": false,
         "no-increment-decrement": false,
         "no-relative-imports": false,
         "no-suspicious-comment": false,
+        "prefer-array-literal": [true, { 'allow-type-parameters': true }],
         "prefer-type-cast": false
     }
 };
