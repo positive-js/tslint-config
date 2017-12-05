01129209220e1eb8f3fdb2b15232bb063dea4f8e
Andrej Chulkov <achulkov@ptsecurity.com>
2017-12-05 16:24:53 +0300

    feat(tslint-config): No console output allowed except .debug

--- src/configs/tslint-override.ts
+++ src/configs/tslint-override.ts
@@ -38,7 +38,7 @@ export default {
             }
         ],
         "radix": false,
-        "no-console": [true, "debug"],
+        "no-console": [true, "info", "warn", "error", "log"],
         "no-duplicate-switch-case": true,
         "no-for-in-array": true,
         "no-implicit-dependencies": [true, "dev"],
