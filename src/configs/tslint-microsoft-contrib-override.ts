bec33dcb188606901facf14c16c66f08f013d9f3
Dkrylova <dkrylova@ptsecurity.com>
2017-12-08 11:43:04 +0300

    feat(tslint-config): change config value for "no-multiline-string"

--- src/configs/tslint-microsoft-contrib-override.ts
+++ src/configs/tslint-microsoft-contrib-override.ts
@@ -6,6 +6,7 @@ export default {
         "function-name": false,
         "missing-jsdoc": false,
         "no-increment-decrement": false,
+        "no-multiline-string": false,
         "no-relative-imports": false,
         "no-suspicious-comment": false,
         "prefer-type-cast": false,
