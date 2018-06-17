9adb9ae8044e79d81d6615ce07903cba9809bc29
Oleg Ulyanov <imrekbg@gmail.com>
2018-06-17 19:04:52 +0300

    feat: removed codelizer rules

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
-import angular from './configs/angular';
 import customRules from './configs/custom-rules';
-import tslintMSContrib from './configs/tslint-microsoft-contrib-override';
+import tslintMicrosoftContribOverride from './configs/tslint-microsoft-contrib-override';
 import tslintOverride from './configs/tslint-override';
+
 
 module.exports = {
     rulesDirectory: [
         './rules/'
     ],
     extends: [
-        'codelyzer',
         'tslint-eslint-rules',
         'tslint-microsoft-contrib',
         'tslint:recommended'
     ],
     rules: {
-        ...angular.rules,
-        ...tslintMSContrib.rules,
+        ...tslintMicrosoftContribOverride.rules,
         ...tslintOverride.rules,
         ...customRules.rules
     }
