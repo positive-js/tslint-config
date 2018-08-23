31e5a7e21fa17a6d9286acad78877d09d83955d5
Oleg Pimenov <pimenov.o@gmail.com>
2018-08-23 23:20:53 +0300

    chore: added notify

---
+++ scripts/trigger_notify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl \
+-i \
+-X POST \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-d 'payload={
+  "channel": "test-bot",
+  "username": "igorek-bot",
+  "text": "#### TSLint Config was published.\n
+  | Component  | Tests Run   | Tests Failed                                   |
+  |:-----------|:-----------:|:-----------------------------------------------|
+  | Server     | 948         | :white_check_mark: 0                           |
+  | Web Client | 123         | :warning: 2 [(see details)](http://linktologs) |
+  | iOS Client | 78          | :warning: 3 [(see details)](http://linktologs) |
+  "
+}' https://chat.ptsecurity.com/hooks/$MATTER_UI-BOT-HOOK
+
