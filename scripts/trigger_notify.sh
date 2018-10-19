c96a4c10cd70a8b18615a7469f1cd5268b3b3a60
Oleg Ulyanov <imrekbg@gmail.com>
2018-10-19 09:51:49 +0300

    chore: change post to mattermost

--- scripts/trigger_notify.sh
+++ scripts/trigger_notify.sh
@@ -5,13 +5,17 @@ set -e
 echo "Matter token"
 echo ${UI_BOT_TOKEN}
 
+VERSION_CURRENT_PACKAGE=$(npm info @ptsecurity/tslint-config version)
+
 curl \
 -i \
 -X POST \
 -H 'Content-Type: application/x-www-form-urlencoded' \
 -d 'payload={
-  "channel": "town-square",
-  "username": "igorek-bot",
-  "text": "#### TSLint Config was published.\n"
+  "channel": "Earth",
+  "username": "Wall-e",
+  "text": "#### TSLint Config was published.\n
+  [(tslint-config@${VERSION_CURRENT_PACKAGE})](https://www.npmjs.com/package/@ptsecurity/tslint-config)\n
+  [(View changelog)](https://github.com/positive-js/tslint-config/blob/master/CHANGELOG.md)"
 }' https://chat.ptsecurity.com/hooks/${UI_BOT_TOKEN}
 
