90c5b12757c7aa2cd1d1ea9f1325a077fdf351ca
RistiCore <risticore@mail.ee>
2018-10-29 15:08:05 +0300

    fix: Remove deprecated rules and some unuseful restrictions

--- src/configs/rxjs-override.ts
+++ src/configs/rxjs-override.ts
@@ -2,8 +2,6 @@ export const rxjsOverrideConfig = {
     rules: {
         'rxjs-no-add': { severity: 'error' },
         'rxjs-no-patched': { severity: 'error' },
-        'rxjs-no-wholesale': { severity: 'error' },
-        'rxjs-no-finnish': { severity: 'error' },
         'rxjs-no-internal': { severity: 'error' }
     }
 };
